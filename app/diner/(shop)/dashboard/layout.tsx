e3aaef9a7f43fe59e3d8619892ac6683b83fd310
Kwaku28 <rekenburg777@gmail.com>
2025-02-04 20:47:15 +0000

    Define the layout for the dashboard

---
+++ app/diner/(shop)/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import ProcessOrder from "@/components/dashboard/ProcessOrder";
+
+export const metadata = {
+  title: "BB Bunch",
+  description: "Grab a Bunch, Start Your Day Right.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="md:grid md:grid-cols-4 w-full h-[89vh]">
+      <div className="md:col-span-3">
+        {children}
+      </div>
+
+      <div className="border-l p-3 hidden md:block">
+        <ProcessOrder />
+      </div>
+    </div>
+  );
+}
