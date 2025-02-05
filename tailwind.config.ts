ea5e588e2490ff118b14eee244618986fa8ac65b
Kwaku28 <rekenburg777@gmail.com>
2025-02-05 04:24:43 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,10 +91,15 @@ const config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+				move: {
+          "0%": { transform: "translateX(-200px)" },
+          "100%": { transform: "translateX(200px)" },
+        },
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
+				move: "move 5s linear infinite",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
   		}
